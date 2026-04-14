b790ea589ab066640fb4c8b862b41a79bb2f3533
neya-mul <neyamulislam946@gmail.com>
2026-04-14 10:28:57 +0600

    vercel file added

--- src/Components/Friend.jsx
+++ src/Components/Friend.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 export default function Friend({friend}) {
   return (
     <div className='p-7 text-center max-w-[300px] min-w-[300px] border mx-auto bg-white rounded-2xl border-gray-400 space-y-4'>
-
         <img src={friend.picture} alt=""  className='mx-auto rounded-full'/>
         <h1>{friend.name}</h1>
         <p>{friend.days_since_contact} Days ago</p>
